9d9ced95c948134f14121d1c3dfad603368427cb
RozimuhammadIsomiddinov <roziisomiddinov@gmail.com>
2025-03-04 20:07:58 +0500

    added

--- controllers/payments/model.js
+++ controllers/payments/model.js
@@ -1,25 +1,4 @@
 const pool = require("../../config/dbconfig");
-const cron = require("node-cron");
-
-// Har oyning 1-kuni soat 00:00 da payment_status ni false qilish
-cron.schedule("0 0 1 * *", async () => {
-  try {
-    const result = await pool.query(`
-      UPDATE users
-      SET payment_status = false
-      WHERE (SELECT MAX(payment_date) FROM payment WHERE payment.user_id = users.id) < DATE_TRUNC('month', NOW())
-      RETURNING *;
-    `);
-
-    if (result.rows.length > 0) {
-      console.log("Payment status updated for users:", result.rows);
-    } else {
-      console.log("No users found for payment status reset.");
-    }
-  } catch (err) {
-    console.error("Error resetting payment status:", err.message);
-  }
-});
 
 const paymentHistoryQuery = `
  SELECT
