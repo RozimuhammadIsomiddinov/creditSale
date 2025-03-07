aa54da41ffd568a9f0acd716b081533e94feb0c0
RozimuhammadIsomiddinov <roziisomiddinov@gmail.com>
2025-03-07 16:57:54 +0500

    added

--- controllers/collector/model.js
+++ controllers/collector/model.js
@@ -48,7 +48,7 @@ SELECT
 FROM payment p
 JOIN collector c ON p.collector_id = c.id
 JOIN zone z ON p.zone_id = z.id
-WHERE DATE(p.payment_date) = CURRENT_DATE  -- Faqat bugungi sana bilan mos keladigan to‘lovlar
+WHERE DATE(p.payment_date) = CURRENT_DATE  -- Faqat bugungi sana bilan mos keladigan tolovlar
 GROUP BY z.zone_name, c.login, c.id, day
 ORDER BY day DESC, total_collected DESC;
 
