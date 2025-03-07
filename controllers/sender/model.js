99add6ece72d6c9b8c63badcd582e1f6edf1eb72
RozimuhammadIsomiddinov <roziisomiddinov@gmail.com>
2025-03-07 11:34:03 +0500

    added

--- controllers/sender/model.js
+++ controllers/sender/model.js
@@ -2,10 +2,11 @@ const pool = require("../../config/dbconfig");
 
 const selectByZone = `
  SELECT u.id, u.name, u.product_name, u.cost, 
-       u.phone_number, u.phone_number2, u.workplace,
+       u.phone_number, u.phone_number2, u.zone AS zone_name, u.workplace AS workplace_name,
        u.payment, u.given_day
 FROM users AS u 
-JOIN zone AS z ON z.id = u.zone
+JOIN zone AS z ON u.id = z.zone
+JOIN workplace AS w ON u.id = w.workplace
 WHERE z.zone_name ILIKE $1;
 
 `;
