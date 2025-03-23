04d4927a71bd3e3c5b6faeffe673eafe79ef909b
RozimuhammadIsomiddinov <roziisomiddinov@gmail.com>
2025-03-23 22:10:48 +0500

    added

--- migrations/admin.js
+++ migrations/admin.js
@@ -28,7 +28,32 @@ const dropAdminTable = async () => {
     console.error("Error dropping table:", e.message);
   }
 };
+const create_index = async () => {
+  try {
+    await pool.query(`
+      CREATE INDEX IF NOT EXISTS idx_payment_user_id_date 
+      ON payment(user_id, payment_date DESC);
+    `);
+    await pool.query(`
+      CREATE INDEX IF NOT EXISTS idx_users_zone_recycle_workplace_paymentstatus 
+      ON users(zone, recycle, workplace, payment_status);
+    `);
+    await pool.query(`
+      CREATE INDEX IF NOT EXISTS idx_users_recycle_true 
+      ON users(recycle);
+    `);
+    await pool.query(`
+      CREATE INDEX IF NOT EXISTS idx_users_fully_paid 
+      ON users(id) 
+      WHERE payment >= cost AND recycle = false;
+    `);
+    return "All indexes created successfully!";
+  } catch (e) {
+    console.error("Error creating indexes:", e.message);
+  }
+};
 
+create_index().then((a) => console.log(a));
 module.exports = {
   createAdminTable,
   dropAdminTable,
