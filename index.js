57bc25863d8a5df46676574eff2a278a2c409335
RozimuhammadIsomiddinov <roziisomiddinov@gmail.com>
2025-07-01 10:20:17 +0500

    added

--- index.js
+++ index.js
@@ -18,13 +18,17 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: "https://credit-sale-front.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+    optionsSuccessStatus: 200,
   })
 );
+
+app.options("*", cors());
 const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 const { auth } = require("./middleware/auth");
