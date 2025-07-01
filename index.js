6e4be6fa91a0f6a75f3c63cde020f276c06ceaf9
RozimuhammadIsomiddinov <roziisomiddinov@gmail.com>
2025-07-01 11:24:42 +0500

    added

--- index.js
+++ index.js
@@ -18,17 +18,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-    optionsSuccessStatus: 200,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
-
-app.options("*", cors());
 const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 const { auth } = require("./middleware/auth");
