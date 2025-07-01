aedc8087de3f85f250d0846b38a8f52b8c580109
RozimuhammadIsomiddinov <roziisomiddinov@gmail.com>
2025-07-01 11:14:20 +0500

    added

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://credit-sale-front.vercel.app",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
